614213704ab433e1101ae0067b2020b4d48729f9
yehaicheng20050066 <yhaicheng@kinlong.cn>
2020-06-24 11:47:20 +0800

    0624搭建好redisson环境，方便实战

---
+++ src/main/java/com/redis/demo/test/Test.java
@@ -0,0 +1,15 @@
+package com.redis.demo.test;
+
+import org.redisson.api.RedissonClient;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+public class Test {
+    @Autowired
+    RedissonClient redisson;
+    /**
+     *    后期将使用Redissonclient 进行实战练习，了解RedissonClient能解决什么问题，怎么去实现解决这个问题
+     *
+     * **/
+
+}
